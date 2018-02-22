e92ac77583df1ca3256ad71af97ed0d8317ac9ad
Vitaliy-1 <vitaliybezsh@gmail.com>
2018-02-22 22:22:38 +0200

    initial commit

--- src/classes/back/AbstractReference.inc.php
+++ src/classes/back/AbstractReference.inc.php
@@ -1,6 +1,6 @@
 <?php namespace JATSParser;
 
-require_once (__DIR__ . "/../../interfaces/Reference.inc.php");
+require_once(__DIR__ . "/../../interfaces/Reference.inc.php");
 
 use JATSParser\Reference as Reference;
 use JATSParser\Document as Document;
@@ -13,15 +13,17 @@ abstract class AbstractReference implements Reference {
 
 	abstract public function getTitle();
 
-    abstract public function getAuthors();
+	abstract public function getAuthors();
 
 	abstract public function getYear();
 
-	protected function __construct() {
+	protected function __construct()
+	{
 		$this->xpath = Document::getXpath();
 	}
 
-	protected function extractOneProperty(\DOMElement $reference, string $xpathExpression) {
+	protected function extractOneProperty(\DOMElement $reference, string $xpathExpression)
+	{
 		$property = null;
 		$searchNodes = $this->xpath->query($xpathExpression, $reference);
 		if ($searchNodes->length > 0) {
@@ -32,13 +34,14 @@ abstract class AbstractReference implements Reference {
 		return $property;
 	}
 
-	protected function extractPubId(\DOMElement $reference, string $pubIdName) {
+	protected function extractPubId(\DOMElement $reference, string $pubIdName)
+	{
 		$pubIdValue = null;
-		$pubIdNodes = $this->xpath->query(".//pub-id" , $reference);
+		$pubIdNodes = $this->xpath->query(".//pub-id", $reference);
 		if ($pubIdNodes->length > 0) {
 			foreach ($pubIdNodes as $pubIdNode) {
 				/* @var $pubIdNode \DOMElement */
-				if($pubIdNode->hasAttribute("pub-id-type")) {
+				if ($pubIdNode->hasAttribute("pub-id-type")) {
 					$pubAttributeValue = $pubIdNode->getAttribute("pub-id-type");
 					if ($pubAttributeValue == $pubIdName) {
 						$pubIdValue = $pubIdNode->nodeValue;
@@ -49,7 +52,8 @@ abstract class AbstractReference implements Reference {
 		return $pubIdValue;
 	}
 
-	protected function extractId(\DOMElement $reference){
+	protected function extractId(\DOMElement $reference)
+	{
 		$id = null;
 		if ($reference->hasAttribute("id")) {
 			$id = $reference->getAttribute("id");
@@ -57,12 +61,13 @@ abstract class AbstractReference implements Reference {
 		return $id;
 	}
 
-	protected function extractAuthors(\DOMElement $reference) {
+	protected function extractAuthors(\DOMElement $reference)
+	{
 		$authors = array();
 		$ifAuthors = $this->xpath->query(".//person-group", $reference);
 		foreach ($ifAuthors as $ifAuthor) {
 			/* @var $ifAuthor \DOMElement */
-			if (!$ifAuthor->hasAttribute("person-group-type") || $ifAuthor->getAttribute("person-group-type") == "author")  {
+			if (!$ifAuthor->hasAttribute("person-group-type") || $ifAuthor->getAttribute("person-group-type") == "author") {
 				$authorNodes = $this->xpath->query(".//name|.//collab", $ifAuthor);
 				if ($authorNodes->length > 0) {
 					foreach ($authorNodes as $authorNode) {
@@ -75,12 +80,13 @@ abstract class AbstractReference implements Reference {
 		return $authors;
 	}
 
-	protected function extractEditors(\DOMElement $reference) {
+	protected function extractEditors(\DOMElement $reference)
+	{
 		$editors = array();
 		$ifEditors = $this->xpath->query(".//person-group", $reference);
 		foreach ($ifEditors as $ifEditor) {
 			/* @var $ifEditor \DOMElement */
-			if ($ifEditor->getAttribute("person-group-type") == "editor")  {
+			if ($ifEditor->getAttribute("person-group-type") == "editor") {
 				$authorNodes = $this->xpath->query(".//name|.//collab", $ifEditor);
 				if ($authorNodes->length > 0) {
 					foreach ($authorNodes as $authorNode) {
