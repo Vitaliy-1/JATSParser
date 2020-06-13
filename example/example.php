9fe7c719ef64955badfbec2708c0179c1f5d3187
marciuz <marcelloverona@gmail.com>
2020-06-13 11:41:21 +0200

    moved the example in a separate dir

--- example/example.php
+++ example/example.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use JATSParser\Body\Document as JATSDocument;
 use JATSParser\HTML\Document as HTMLDocument;
